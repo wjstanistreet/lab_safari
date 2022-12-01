9969c92fcd8f76d8687cc2c592fe99a7316280a2
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-12-01 16:49:28 +0000

    completed mvp

--- safari_queries.sql
+++ safari_queries.sql
@@ -1,7 +1,13 @@
 -- queries go here
 	-- - The names of the animals in a given enclosure
-	-- - The names of the staff working in a given enclosure
 
 SELECT animals.name AS Animal_name, enclosures.name AS Enclosure_name FROM animals 
 join enclosures on animals.enclosure_id = enclosures.id
 WHERE enclosure_id = 1;
+
+	-- - The names of the staff working in a given enclosure
+
+SELECT staff.name AS staff_name, enclosures.name AS enclosure_name FROM assignments 
+JOIN staff ON assignments.employee_id = staff.id 
+JOIN enclosures ON assignments.enclosure_id = enclosures.id
+WHERE enclosure_id = 2;
