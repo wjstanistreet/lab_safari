8c9b39ce74ad99271052038f8ee6d403d8591ab8
Wills BNTA Macbook <williamstanistreet0299@gmail.com>
2022-12-01 15:47:11 +0000

    added ERD table base

--- safari.sql
+++ safari.sql
@@ -1,9 +1,35 @@
 -- database creation and data insertion goes here
 
+DROP TABLE assignments;
 DROP TABLE animals;
+DROP TABLE staff;
+DROP TABLE enclosures;
+
+
+
+CREATE TABLE enclosures (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    capacity INT,
+    closedForMaintenance BOOLEAN
+);
+
+CREATE TABLE staff (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    employee_number INT
+);
 
 CREATE TABLE animals (
     id SERIAL,
     name VARCHAR(255),
-    age INT
+    age INT,
+    enclosure_id INT REFERENCES enclosures(id)
+);
+
+CREATE TABLE assignments (
+    id SERIAL,
+    employee_id INT REFERENCES staff(id),
+    enclosure_id INT REFERENCES enclosures(id),
+    day VARCHAR(255)
 );
